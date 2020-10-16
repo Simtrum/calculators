0759f9a9b2efe85b35a4b82a08fb525f689ad458
wensiannn <gohwensian@gmail.com>
2020-10-16 15:22:20 +0800

    commit

---
+++ search/search.js
@@ -0,0 +1,14 @@
+function search_animal() {
+    let input = document.getElementById('searchButton').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('animals');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";
+        }
+    }
+}
